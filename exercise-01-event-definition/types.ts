72e73150376ba2db8af4696952761c8c75035ddf
Alex Gates-Shannon <alex.nebula99@gmail.com>
2022-06-28 10:12:20 -0400

    WIN-21: Complete my answers for exercise 1

--- exercise-01-event-definition/types.ts
+++ exercise-01-event-definition/types.ts
@@ -1,10 +1,21 @@
 // --- TYPE CONSTANTS ---
 
-type SHOPPING_CART_CREATED = 'event-type.shopping-cart-created';
+export const PRODUCT_ADDED_TO_CART = 'event-type.product-added-to-cart' as const;
+export const PRODUCT_REMOVED_FROM_CART = 'event-type.product-removed-from-cart' as const;
+export const SHOPPING_CART_CANCELED = 'event-type.shopping-cart-canceled' as const;
+export const SHOPPING_CART_CONFIRMED = 'event-type.shopping-cart-confirmed' as const;
+export const SHOPPING_CART_CREATED = 'event-type.shopping-cart-created' as const;
+export const SHOPPING_CART_FULFILLMENT_STARTED = 'event-type.shopping-cart-fulfillment-stated';
+export const SHOPPING_CART_STATUS = {
+  CLOSED: 'shopping-cart-status.closed',
+  OPEN: 'shopping-cart-status.open',
+} as const;
 
 // --- TYPE ALIASES ---
 
 type CartId = string;
+type InvoiceId = string;
+type ProductId = string;
 type UserId = string;
 
 // --- EVENTS ---
@@ -17,15 +28,58 @@ type BaseEvent<
   data: Readonly<EventData>;
 }>;
 
-type ShoppingCartCreatedEvent = BaseEvent<
-  SHOPPING_CART_CREATED,
+export type CartFulfillmentStarted = BaseEvent<
+  typeof SHOPPING_CART_FULFILLMENT_STARTED,
   {
-    userId: UserId,
-    shoppingCartId: CartId
+    invoiceId: InvoiceId,
+    shoppingCartId: CartId,
   }
 >;
 
-type ProductAddedToCart = {};
-type ProductRemovedFromCart = {};
-type ShoppingCartConfirmed = {};
-type ShoppingCartCanceled = {};
+export type ProductAddedToCart = BaseEvent<
+  typeof PRODUCT_ADDED_TO_CART,
+  {
+    productId: ProductId,
+    quantity: number,
+    shoppingCartId: CartId,
+    unitPrice: number,
+  }
+>;
+
+export type ProductRemovedFromCart = BaseEvent<
+  typeof PRODUCT_REMOVED_FROM_CART,
+  {
+    productId: ProductId,
+    quantity: number,
+    shoppingCartId: CartId,
+    unitPrice: number,
+  }
+>;
+
+export type ShoppingCartCanceled = BaseEvent<
+  typeof SHOPPING_CART_CANCELED,
+  {
+    shoppingCartId: CartId,
+    status: typeof SHOPPING_CART_STATUS.CLOSED,
+    userId: UserId,
+  }
+>;
+
+export type ShoppingCartConfirmed = BaseEvent<
+  typeof SHOPPING_CART_CONFIRMED,
+  {
+    shoppingCartId: CartId,
+    status: typeof SHOPPING_CART_STATUS.CLOSED,
+    userId: UserId,
+  }
+>;
+
+export type ShoppingCartCreatedEvent = BaseEvent<
+  typeof SHOPPING_CART_CREATED,
+  {
+    shoppingCartId: CartId,
+    status: typeof SHOPPING_CART_STATUS.OPEN,
+    userId: UserId,
+  }
+  >;
+
