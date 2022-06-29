a275c1e76bbaefe7f87bd7fe991fd897737b7ef0
Alex Gates-Shannon <alex.nebula99@gmail.com>
2022-06-29 13:47:29 -0400

    WIN-24: Add test data

--- exercise-02-getting-state-from-events/src/index.test.ts
+++ exercise-02-getting-state-from-events/src/index.test.ts
@@ -1,17 +1,79 @@
 import { expect } from 'chai';
 import { getShoppingCart } from './index';
-import { BaseEvent, PricedProduct } from './types';
+import {
+  PRODUCT_ADDED_TO_CART,
+  PRODUCT_REMOVED_FROM_CART,
+  SHOPPING_CART_CREATED,
+  SHOPPING_CART_STATUS,
+  BaseEvent,
+  PricedProduct,
+  ProductAddedToCart,
+  ProductRemovedFromCart,
+  ShoppingCartCreatedEvent,
+} from './types';
 
 describe('Function `getShoppingCart`', () => {
   function getValues() {
-    const events: BaseEvent[] = [];
-    const items: PricedProduct[] = [];
-    return {
-      events,
-      items,
-      cartId: '6f79884e-c74b-4032-b7e9-42f3bd93ad28',
-      userId: '0f89fe17-658c-43f8-8712-cf52d540228e',
+    const cartId = '6f79884e-c74b-4032-b7e9-42f3bd93ad28';
+    const userId = '0f89fe17-658c-43f8-8712-cf52d540228e';
+
+    const newBenSherman: PricedProduct = {
+      id: '438067bb-5dd8-44e1-9096-387775dbf39b',
+      quantity: 2,
+      unitPrice: 12995,
     };
+    const sweetKicks: PricedProduct = {
+      id: '132ef3bd-a27f-47a3-8af9-9ef77e40ee13',
+      quantity: 1,
+      unitPrice: 22727,
+    };
+    const tightJeans: PricedProduct = {
+      id: 'da01a0b7-6a0d-4443-9b55-6cd61569a449',
+      quantity: 1,
+      unitPrice: 8489,
+    };
+
+    const event1: ShoppingCartCreatedEvent = {
+      type: SHOPPING_CART_CREATED,
+      data: {
+        userId,
+        shoppingCartId: cartId,
+        status: SHOPPING_CART_STATUS.PENDING,
+      },
+    };
+    const event2: ProductAddedToCart = {
+      type: PRODUCT_ADDED_TO_CART,
+      data: {
+        product: sweetKicks,
+        shoppingCartId: cartId,
+      },
+    };
+    const event3: ProductAddedToCart = {
+      type: PRODUCT_ADDED_TO_CART,
+      data: {
+        product: tightJeans,
+        shoppingCartId: cartId,
+      },
+    };
+    const event4: ProductAddedToCart = {
+      type: PRODUCT_ADDED_TO_CART,
+      data: {
+        product: newBenSherman,
+        shoppingCartId: cartId,
+      },
+    };
+    const event5: ProductRemovedFromCart = {
+      type: PRODUCT_REMOVED_FROM_CART,
+      data: {
+        product: tightJeans,
+        shoppingCartId: cartId,
+      },
+    };
+
+    const events: BaseEvent[] = [event1, event2, event3, event4, event5];
+    const items: PricedProduct[] = [sweetKicks, newBenSherman];
+
+    return { cartId, events, items, userId };
   }
 
   it('returns the correct shopping cart ID', () => {
