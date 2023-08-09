9109b9947830bd1aa4483d6eaaaa96f362278f76
Alex Gates-Shannon <alex.nebula99@gmail.com>
2023-08-09 17:54:01 -0400

    Initialize exercise 3

---
+++ exercises/exercise-03-appending-events-to-eventstoredb/shopping-cart-reducer.test.ts
@@ -0,0 +1,37 @@
+import {
+  cartConfirmed,
+  cartOpened,
+  greenBallsAdded,
+  greenBallsRemoved,
+  redBalls,
+  redBallsAdded,
+  yellowBall,
+  yellowBallAdded,
+} from './events';
+import { shoppingCardReducer } from './shopping-cart-reducer';
+import { ShoppingCart, ShoppingCartEvent } from './types';
+
+describe('Shopping cart reducer', () => {
+  it('correctly processes a series of events', () => {
+    const expectedOutput: ShoppingCart = {
+      id: 'cart-7',
+      clientId: 'client-19',
+      confirmedAt: new Date('2023-08-07'),
+      openedAt: new Date('2023-08-06'),
+      products: [redBalls, yellowBall],
+      status: 'confirmed',
+    };
+
+    const events: ShoppingCartEvent[] = [
+      cartOpened,
+      redBallsAdded,
+      greenBallsAdded,
+      yellowBallAdded,
+      greenBallsRemoved,
+      cartConfirmed,
+    ];
+    const cart = shoppingCardReducer(events);
+
+    expect(cart).toEqual(expectedOutput);
+  });
+});
