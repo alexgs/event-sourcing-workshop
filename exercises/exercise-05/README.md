06acec88e16f70361cd8bc0c3832b730a519e967
Alex Gates-Shannon <alex.nebula99@gmail.com>
2023-08-12 13:11:17 -0400

    Initialize exercise 6

---
+++ exercises/exercise-05/README.md
@@ -0,0 +1,7 @@
+# Exercise 05
+
+[This exercise][1] seems to be a little incomplete in Oskar's repo. I think
+that everything in this exercise is also included in exercise 6. So I'm going
+to skip this one and just move on.
+
+[1]: https://github.com/oskardudycz/EventSourcing.NodeJS/tree/main/workshops/introduction_to_event_sourcing/src/05_business_logic
---
+++ exercises/exercise-06-business-logic/get-shopping-cart.test.ts
@@ -0,0 +1,53 @@
+import { EventStoreDBClient } from '@eventstore/db-client';
+import { appendToStream } from './append-to-stream';
+import { CLIENT_ID, SHOPPING_CART_ID } from './constants';
+import {
+  cartConfirmed,
+  cartOpened,
+  greenBallsAdded,
+  greenBallsRemoved,
+  redBalls,
+  redBallsAdded,
+  yellowBall,
+  yellowBallAdded,
+} from './events';
+import { getShoppingCart } from './get-shopping-cart';
+import {ShoppingCart, ShoppingCartEvent} from './types';
+
+describe('Function `getShoppingCart`', () => {
+  let eventStore: EventStoreDBClient = null;
+
+  beforeAll(async () => {
+    eventStore = EventStoreDBClient.connectionString(
+      'esdb://localhost:2113?tls=false',
+    );
+  });
+
+  afterAll(async () => {
+    await eventStore.dispose();
+  });
+
+  it('correctly retrieves the state of the shopping cart', async () => {
+    const streamName = `shopping-cart-${SHOPPING_CART_ID}`;
+    const events: ShoppingCartEvent[] = [
+      cartOpened,
+      redBallsAdded,
+      greenBallsAdded,
+      yellowBallAdded,
+      greenBallsRemoved,
+      cartConfirmed,
+    ];
+    await appendToStream(eventStore, streamName, events);
+
+    const output = await getShoppingCart(eventStore, streamName);
+    const expectedOutput: ShoppingCart = {
+      id: SHOPPING_CART_ID,
+      clientId: CLIENT_ID,
+      confirmedAt: new Date('2023-08-07'),
+      openedAt: new Date('2023-08-06'),
+      products: [redBalls, yellowBall],
+      status: 'confirmed',
+    };
+    expect(output).toEqual(expectedOutput);
+  });
+});
