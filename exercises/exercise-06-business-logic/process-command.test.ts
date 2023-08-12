78681b568963d8580c04f4e2e3371f44adb88e66
Alex Gates-Shannon <alex.nebula99@gmail.com>
2023-08-12 15:13:34 -0400

    Add tests and implementations for remaining command processor functions

--- exercises/exercise-06-business-logic/process-command.test.ts
+++ exercises/exercise-06-business-logic/process-command.test.ts
@@ -1,6 +1,18 @@
-import { CLIENT_ID, RED_BALLS, SHOPPING_CART_ID } from './constants';
+import {
+  CLIENT_ID,
+  RED_BALLS,
+  SHOPPING_CART_ID,
+  YELLOW_BALL,
+} from './constants';
 import { processCommand } from './process-command';
-import { ProductAddedToCart, ShoppingCart } from './types';
+import {
+  ProductAddedToCart,
+  ProductRemovedFromCart,
+  ShoppingCart,
+  ShoppingCartCanceled,
+  ShoppingCartConfirmed,
+  ShoppingCartOpened,
+} from './types';
 
 describe('Function `processCommand`', () => {
   it('returns the correct event for command `AddProductToCart`', () => {
@@ -30,4 +42,122 @@ describe('Function `processCommand`', () => {
     };
     expect(output).toMatchObject(expectedEvent);
   });
+
+  it('returns the correct event for command `CancelShoppingCart`', () => {
+    const openTime = new Date('2023-08-06');
+    const cancelTime = new Date('2023-08-08');
+
+    const expectedEvent: Partial<ShoppingCartCanceled> = {
+      type: 'shopping-cart-canceled',
+      data: {
+        canceledAt: cancelTime,
+        shoppingCartId: SHOPPING_CART_ID,
+      },
+    };
+    const cart: ShoppingCart = {
+      id: SHOPPING_CART_ID,
+      clientId: CLIENT_ID,
+      openedAt: openTime,
+      products: [],
+      status: 'open',
+    };
+
+    const output = processCommand(cart, {
+      type: 'command.cancel-shopping-cart',
+      data: {
+        timestamp: cancelTime,
+        shoppingCartId: SHOPPING_CART_ID,
+      },
+    });
+    expect(output).toMatchObject(expectedEvent);
+  });
+
+  it('returns the correct event for command `ConfirmShoppingCart`', () => {
+    const openTime = new Date('2023-08-06');
+    const confirmationTime = new Date('2023-08-08');
+
+    const expectedEvent: Partial<ShoppingCartConfirmed> = {
+      type: 'shopping-cart-confirmed',
+      data: {
+        confirmedAt: confirmationTime,
+        shoppingCartId: SHOPPING_CART_ID,
+      },
+    };
+    const cart: ShoppingCart = {
+      id: SHOPPING_CART_ID,
+      clientId: CLIENT_ID,
+      openedAt: openTime,
+      products: [YELLOW_BALL, RED_BALLS],
+      status: 'open',
+    };
+
+    const output = processCommand(cart, {
+      type: 'command.confirm-shopping-cart',
+      data: {
+        timestamp: confirmationTime,
+        shoppingCartId: SHOPPING_CART_ID,
+      },
+    });
+    expect(output).toMatchObject(expectedEvent);
+  });
+
+  it('returns the correct event for command `OpenShoppingCart`', () => {
+    const openTime = new Date('2023-08-06');
+
+    const expectedEvent: Partial<ShoppingCartOpened> = {
+      type: 'shopping-cart-opened',
+      data: {
+        clientId: CLIENT_ID,
+        openedAt: openTime,
+        shoppingCartId: SHOPPING_CART_ID,
+      },
+    };
+    const cart: ShoppingCart = {
+      id: SHOPPING_CART_ID,
+      clientId: CLIENT_ID,
+      openedAt: openTime,
+      products: [],
+      status: 'open',
+    };
+
+    const output = processCommand(cart, {
+      type: 'command.open-shopping-cart',
+      data: {
+        clientId: CLIENT_ID,
+        shoppingCartId: SHOPPING_CART_ID,
+        timestamp: openTime,
+      },
+    });
+    expect(output).toMatchObject(expectedEvent);
+  });
+
+  it('returns the correct event for command `RemoveProductFromCart`', () => {
+    const openTime = new Date('2023-08-06');
+
+    const expectedEvent: Partial<ProductRemovedFromCart> = {
+      type: 'product-removed-from-shopping-cart',
+      data: {
+        productItem: RED_BALLS,
+        shoppingCartId: SHOPPING_CART_ID,
+      },
+    };
+    const cart: ShoppingCart = {
+      id: SHOPPING_CART_ID,
+      clientId: CLIENT_ID,
+      openedAt: openTime,
+      products: [ RED_BALLS, YELLOW_BALL ],
+      status: 'open',
+    };
+
+    const output = processCommand(cart, {
+      type: 'command.remove-product-from-cart',
+      data: {
+        productItem: RED_BALLS,
+        shoppingCartId: SHOPPING_CART_ID,
+      },
+    });
+    expect(output).toMatchObject(expectedEvent);
+  });
+
+  // TODO Add tests for rejecting commands
 });
--- exercises/exercise-06-business-logic/process-command.ts
+++ exercises/exercise-06-business-logic/process-command.ts
@@ -1,17 +1,25 @@
 import { v4 as uuid } from 'uuid';
 import {
   AddProductToCart,
+  CancelShoppingCart,
+  ConfirmShoppingCart,
+  OpenShoppingCart,
   ProductAddedToCart,
+  ProductRemovedFromCart,
+  RemoveProductFromCart,
   ShoppingCart,
+  ShoppingCartCanceled,
   ShoppingCartCommand,
+  ShoppingCartConfirmed,
   ShoppingCartEvent,
+  ShoppingCartOpened,
 } from './types';
 
 function processAddProductToShoppingCart(
   cart: ShoppingCart,
   command: AddProductToCart,
 ): ProductAddedToCart {
-  if (!cart.openedAt) {
+  if (cart.status !== 'open') {
     throw new Error('Cart must be opened before adding product(s).');
   }
 
@@ -25,24 +33,95 @@ function processAddProductToShoppingCart(
   };
 }
 
+function processCancelShoppingCart(
+  cart: ShoppingCart,
+  command: CancelShoppingCart,
+): ShoppingCartCanceled {
+  if (cart.status !== 'open') {
+    throw new Error('Cart must be opened before canceling.');
+  }
+
+  return {
+    id: uuid(),
+    type: 'shopping-cart-canceled',
+    data: {
+      shoppingCartId: command.data.shoppingCartId,
+      canceledAt: command.data.timestamp,
+    },
+  };
+}
+
+function processConfirmShoppingCart(
+  cart: ShoppingCart,
+  command: ConfirmShoppingCart,
+): ShoppingCartConfirmed {
+  if (cart.status !== 'open') {
+    throw new Error('Cart must be opened before confirming.');
+  }
+  if (cart.products.length < 1) {
+    throw new Error('Cart must contain product(s) before confirming.');
+  }
+
+  return {
+    id: uuid(),
+    type: 'shopping-cart-confirmed',
+    data: {
+      shoppingCartId: command.data.shoppingCartId,
+      confirmedAt: command.data.timestamp,
+    },
+  };
+}
+
+function processOpenShoppingCart(
+  _cart: ShoppingCart,
+  command: OpenShoppingCart,
+): ShoppingCartOpened {
+  return {
+    id: uuid(),
+    type: 'shopping-cart-opened',
+    data: {
+      clientId: command.data.clientId,
+      openedAt: command.data.timestamp,
+      shoppingCartId: command.data.shoppingCartId,
+    },
+  };
+}
+
+function processRemoveProductFromCart(
+  cart: ShoppingCart,
+  command: RemoveProductFromCart,
+): ProductRemovedFromCart {
+  if (cart.status !== 'open') {
+    throw new Error('Cart must be opened before removing products.');
+  }
+  if (!cart.products.includes(command.data.productItem)) {
+    throw new Error('Cart must contain the product to be removed.');
+  }
+
+  return {
+    id: uuid(),
+    type: 'product-removed-from-shopping-cart',
+    data: {
+      shoppingCartId: command.data.shoppingCartId,
+      productItem: command.data.productItem,
+    },
+  };
+}
+
 export function processCommand(
   cart: ShoppingCart,
   command: ShoppingCartCommand,
 ): ShoppingCartEvent {
-  // process the command to get an event
-  let event: ShoppingCartEvent = null;
   switch (command.type) {
     case 'command.add-product-to-shopping-cart':
       return processAddProductToShoppingCart(cart, command as AddProductToCart);
     case 'command.cancel-shopping-cart':
-      break;
+      return processCancelShoppingCart(cart, command as CancelShoppingCart);
     case 'command.confirm-shopping-cart':
-      break;
+      return processConfirmShoppingCart(cart, command as ConfirmShoppingCart);
     case 'command.open-shopping-cart':
-      break;
+      return processOpenShoppingCart(cart, command as OpenShoppingCart);
     case 'command.remove-product-from-cart':
-      break;
+      return processRemoveProductFromCart(cart, command as RemoveProductFromCart);
   }
-
-  return event;
 }
