0e4c70a7e442f795733aaab18c2ebcb2d33cb2a1
Alex Gates-Shannon <alex.nebula99@gmail.com>
2023-08-16 14:13:57 -0400

    Add happy path test for business logic

--- exercises/exercise-06-business-logic-with-eventstoredb/command-processor.test.ts
+++ exercises/exercise-06-business-logic-with-eventstoredb/command-processor.test.ts
@@ -1,8 +1,16 @@
 import { EventStoreDBClient } from '@eventstore/db-client';
+import { addSeconds } from 'date-fns';
 import { v4 as uuid } from 'uuid';
-import { SHOPPING_CART_ID } from './constants';
-import { OpenShoppingCart } from './types';
-import {commandProcessor} from "./command-processor";
+import { GREEN_BALLS, RED_BALLS, SHOPPING_CART_ID } from './constants';
+import { commandProcessor } from './command-processor';
+import { getShoppingCart } from './get-shopping-cart';
+import { getShoppingCartStreamName } from './get-shopping-cart-stream-name';
+import {
+  AddProductToCart,
+  ConfirmShoppingCart,
+  OpenShoppingCart,
+  ShoppingCart,
+} from './types';
 
 describe('Function `commandProcessor`', () => {
   let eventStore: EventStoreDBClient = null;
@@ -32,6 +40,57 @@ describe('Function `commandProcessor`', () => {
     expect(result).toEqual(1);
   });
 
-  it.todo('handles a (happy path) series of commands');
+  it('handles a (happy path) series of commands', async () => {
+    const now = new Date();
+    const command1: OpenShoppingCart = {
+      type: 'command.open-shopping-cart',
+      data: {
+        shoppingCartId: SHOPPING_CART_ID,
+        clientId: uuid(),
+        timestamp: now,
+      },
+    };
+    const command2: AddProductToCart = {
+      type: 'command.add-product-to-shopping-cart',
+      data: {
+        shoppingCartId: SHOPPING_CART_ID,
+        productItem: RED_BALLS,
+      },
+    };
+    const command3: AddProductToCart = {
+      type: 'command.add-product-to-shopping-cart',
+      data: {
+        shoppingCartId: SHOPPING_CART_ID,
+        productItem: GREEN_BALLS,
+      },
+    };
+    const command4: ConfirmShoppingCart = {
+      type: 'command.confirm-shopping-cart',
+      data: {
+        shoppingCartId: SHOPPING_CART_ID,
+        timestamp: addSeconds(now, 13),
+      },
+    };
+
+    const result1 = await commandProcessor(eventStore, command1);
+    const result2 = await commandProcessor(eventStore, command2);
+    const result3 = await commandProcessor(eventStore, command3);
+    const result4 = await commandProcessor(eventStore, command4);
+
+    const actualCart = await getShoppingCart(
+      eventStore,
+      getShoppingCartStreamName(SHOPPING_CART_ID),
+    );
+    const expectedCart: ShoppingCart = {
+      id: SHOPPING_CART_ID,
+      status: 'confirmed',
+      products: [RED_BALLS, GREEN_BALLS],
+      clientId: command1.data.clientId,
+      openedAt: now,
+      confirmedAt: addSeconds(now, 13),
+    };
+    expect(actualCart).toEqual(expectedCart);
+  });
+
   it.todo('correctly handles violations of business logic rules');
 });
