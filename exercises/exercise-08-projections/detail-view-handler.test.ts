ecdab2416a5393c7fe0f2090084e7d87d595a70e
Alex Gates-Shannon <alex.nebula99@gmail.com>
2023-08-17 10:44:54 -0400

    Add additional unit tests for detail view handler

--- exercises/exercise-08-projections/detail-view-handler.test.ts
+++ exercises/exercise-08-projections/detail-view-handler.test.ts
@@ -30,7 +30,7 @@ describe('Function `detailViewHandler`', () => {
         eventId: uuid(),
         streamPosition: 0,
         logPosition: 0,
-      }
+      },
     };
     detailViewHandler(db)(event);
     const output = db.get(shoppingCartId);
@@ -47,5 +47,121 @@ describe('Function `detailViewHandler`', () => {
     expect(output).toEqual(expectedDetails);
   });
 
-  it.todo('correctly handles adding an item to a shopping cart');
+  describe('correctly handles adding an item to a shopping cart', () => {
+    it('when the item is *not* already in the cart', () => {
+      const clientId = uuid();
+      const db = database.collection<ShoppingCartDetails>('shoppingCarts');
+      const dressId = uuid();
+      const openTimestamp = new Date().toISOString();
+      const shoppingCartId = uuid();
+
+      const emptyCart: ShoppingCartDetails = {
+        id: shoppingCartId,
+        productItems: [],
+        status: ShoppingCartStatus.Pending,
+        clientId: clientId,
+        openedAt: openTimestamp,
+        totalAmount: 0,
+        totalItemsCount: 0,
+      };
+      db.store(shoppingCartId, emptyCart);
+
+      const event = {
+        type: 'ProductItemAddedToShoppingCart',
+        data: {
+          shoppingCartId,
+          productItem: {
+            productId: dressId,
+            quantity: 3,
+            unitPrice: 150,
+          },
+        },
+        metadata: {
+          eventId: uuid(),
+          streamPosition: 1,
+          logPosition: 1,
+        },
+      };
+      detailViewHandler(db)(event);
+      const output = db.get(shoppingCartId);
+
+      const expectedDetails: ShoppingCartDetails = {
+        id: shoppingCartId,
+        productItems: [
+          {
+            productId: dressId,
+            quantity: 3,
+            unitPrice: 150,
+          },
+        ],
+        status: ShoppingCartStatus.Pending,
+        clientId: clientId,
+        openedAt: openTimestamp,
+        totalAmount: 450,
+        totalItemsCount: 3,
+      };
+      expect(output).toEqual(expectedDetails);
+    });
+
+    it('when the item *is* already in the cart', () => {
+      const clientId = uuid();
+      const db = database.collection<ShoppingCartDetails>('shoppingCarts');
+      const dressId = uuid();
+      const openTimestamp = new Date().toISOString();
+      const shoppingCartId = uuid();
+
+      const pendingCart: ShoppingCartDetails = {
+        id: shoppingCartId,
+        productItems: [
+          {
+            productId: dressId,
+            quantity: 3,
+            unitPrice: 150,
+          }
+        ],
+        status: ShoppingCartStatus.Pending,
+        clientId: clientId,
+        openedAt: openTimestamp,
+        totalAmount: 450,
+        totalItemsCount: 3,
+      };
+      db.store(shoppingCartId, pendingCart);
+
+      const event = {
+        type: 'ProductItemAddedToShoppingCart',
+        data: {
+          shoppingCartId,
+          productItem: {
+            productId: dressId,
+            quantity: 3,
+            unitPrice: 150,
+          },
+        },
+        metadata: {
+          eventId: uuid(),
+          streamPosition: 2,
+          logPosition: 2,
+        },
+      };
+      detailViewHandler(db)(event);
+      const output = db.get(shoppingCartId);
+
+      const expectedDetails: ShoppingCartDetails = {
+        id: shoppingCartId,
+        productItems: [
+          {
+            productId: dressId,
+            quantity: 6,
+            unitPrice: 150,
+          },
+        ],
+        status: ShoppingCartStatus.Pending,
+        clientId: clientId,
+        openedAt: openTimestamp,
+        totalAmount: 900,
+        totalItemsCount: 6,
+      };
+      expect(output).toEqual(expectedDetails);
+    });
+  });
 });
